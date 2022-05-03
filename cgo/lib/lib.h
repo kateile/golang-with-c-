2be0507f57e6019a5c910269697e9266ebd84097
Sylvanus Kateile <kateile@outlook.com>
2022-05-03 21:12:38 +0300

    fix some c++ errors

--- cgo/lib/lib.h
+++ cgo/lib/lib.h
@@ -40,11 +40,11 @@ typedef long long GoInt64;
 typedef unsigned long long GoUint64;
 typedef GoInt64 GoInt;
 typedef GoUint64 GoUint;
-typedef __SIZE_TYPE__ GoUintptr;
+typedef __SIZE_TYPE__ GoUintptr; //__SIZE_TYPE__ shows error here
 typedef float GoFloat32;
 typedef double GoFloat64;
-typedef float _Complex GoComplex64;
-typedef double _Complex GoComplex128;
+typedef float _Complex GoComplex64; //GoComplex64 shows error here
+typedef double _Complex GoComplex128; //GoComplex128 shows error here
 
 /*
   static assertion to make sure the file is being used on architecture
