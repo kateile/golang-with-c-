3ba8ad0d39f1731d1544e101d07d99e5ea6cd28f
Sylvanus Kateile <kateile@outlook.com>
2022-05-04 20:14:27 +0300

    do some logging in lib.go

--- cgo/lib.go
+++ cgo/lib.go
@@ -1,9 +1,16 @@
 package main
 
 import "C"
+import (
+	"fmt"
+	"strconv"
+)
 
 //export getHelloWord
 func getHelloWord(number int) int {
+	var str = fmt.Sprintf("Hello world boy %s", strconv.Itoa(number))
+	fmt.Println("number", number)
+	fmt.Println("word", str)
 	return 7888 * number
 }
 
