16c38cf1b12a44407e2c1b859bc4424bdf93596c
Sylvanus Kateile <kateile@outlook.com>
2022-05-04 18:50:32 +0300

    add error handling in c++

--- cgo/cgo.cpp
+++ cgo/cgo.cpp
@@ -3,16 +3,27 @@
 #include <iostream>
 using namespace std;
 
+typedef string(*GW)(string input);
+
 int main()
 {
 	std::cout << "Starting program.\n";
 
 	//Loading dll
-	typedef string (*GW)(string input);
-	HMODULE hModule = LoadLibrary(TEXT("lib/lib.dll"));
+	HINSTANCE  hModule = LoadLibrary(TEXT("lib/lib.dll"));
+
+	if (!hModule) {
+		std::cout << "could not load the dynamic library" << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	GW getHelloWord = (GW)GetProcAddress(hModule, "getHelloWord");
-	
+
+	if (!getHelloWord) {
+		std::cout << "could not locate the function" << std::endl;
+		return EXIT_FAILURE;
+	}
+
 	string output = getHelloWord("Golang");
 
 	//Printing the string returned by above function
